79ea67d2582bdb33c73eaed9dd779a3efaeda6bd
吴飞扬 <21373411@buaa.edu.cn>
2023-02-16 10:01:54 +0800

    Filled blanks in blib.c and machine.c

--- blib.c
+++ blib.c
@@ -1,11 +1,21 @@
 #include <blib.h>
 
 size_t strlen(const char *s) {
-	panic("please implement");
+	size_t size = 0;
+	while (*s != '\0') {
+		size++;
+		s++;
+	}
+	return size;
 }
 
 char *strcpy(char *dst, const char *src) {
-	panic("please implement");
+	char *res = dst;
+	while (*src != '\0') {
+		*dst++ = *src++;
+	}
+	*dst = '\0';
+	return res;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -18,11 +28,29 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-	panic("please implement");
+	char *res = dst;
+	while (*dst != '\0') {
+		dst++;
+	}
+	while (*src != '\0') {
+		*dst++ = *src++;
+	}
+	*dst = '\0';
+	return res;
 }
 
 int strcmp(const char *s1, const char *s2) {
-	panic("please implement");
+	while (*s1 && *s2) {
+		if (*s1 != *s2) {
+			return *s1 - *s2;
+		}
+		s1++;
+		s2++;
+	}
+	if (*s1 || *s2){
+		return *s1 - *s2;
+	}
+	return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -40,7 +68,12 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-	panic("please implement");
+	unsigned char *dst = s;
+	while (n--) {
+		*dst = (unsigned char)c;
+		dst++;
+	}
+	return s;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
@@ -53,5 +86,14 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-	panic("please implement");
+	char *str1 = (char *)s1;
+	char *str2 = (char *)s2;
+	while (n--) {
+		if (*str1 != *str2) {
+			return *str1 - *str2;
+		}
+		str1++;
+		str2++;
+	}
+	return 0;
 }
