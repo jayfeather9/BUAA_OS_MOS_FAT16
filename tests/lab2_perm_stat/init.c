af8335310ae293d6dc7097485a193f6b61575f8a
吴飞扬 <21373411@buaa.edu.cn>
2023-04-03 20:02:37 +0800

    first bug fix try of pp test

--- tests/lab2_perm_stat/init.c
+++ tests/lab2_perm_stat/init.c
@@ -9,18 +9,21 @@ void test_perm_stat() {
 	u_int va[4] = {UTEXT, UTEXT + BY2PG, UTEXT + 1024 * BY2PG, UTEXT + 1025 * BY2PG};
 	u_int perm[4] = {PTE_V | PTE_D, PTE_V | PTE_D | PTE_G, PTE_V | PTE_D | PTE_G,
 			 PTE_V | PTE_G};
-	struct Page *pp;
+	struct Page *pp, *pp2, *pp3, *pp4;
 	for (int i = 0; i < 4; i++){
 		printk("va = %u, PDX = %u, PTX = %u\n", va[i], PDX(va[i]), PTX(va[i]));
 	}
 	assert(page_alloc(&pp) == 0);
+	assert(page_alloc(&pp2) == 0);
+	assert(page_alloc(&pp3) == 0);
+	assert(page_alloc(&pp4) == 0);
 	assert(page_insert(pgdir, 0, pp, va[0], perm[0]) == 0);
-	assert(page_insert(pgdir, 0, pp, va[1], perm[1]) == 0);
-	assert(page_insert(pgdir, 0, pp, va[2], perm[2]) == 0);
+	assert(page_insert(pgdir, 0, pp2, va[1], perm[1]) == 0);
+	assert(page_insert(pgdir, 0, pp3, va[2], perm[2]) == 0);
 	assert(page_insert(pgdir, 0, pp, va[3], perm[3]) == 0);
 	int r = page_perm_stat(pgdir, pp, PTE_D);
 	printk("r = %d\n", r);
-	assert(r == 3);
+	assert(r == 1);
 	printk("test_perm_stat succeeded!\n");
 }
 
